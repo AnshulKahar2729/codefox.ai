80364c31674be546c98bb973fa09bbf03449f713
Anshul Kahar <22511126.dypit@dypvp.edu.in>
2025-03-14 04:14:30 +0530

    Add octokit dependency and update feedback posting URL construction in postFeedback function

--- src/app/actions/feedback.ts
+++ src/app/actions/feedback.ts
@@ -4,18 +4,25 @@ export async function postFeedback(
   prUrl: string,
   feedback: string
 ): Promise<void> {
-  const commentUrl = `${prUrl}/comments`;
   const body = `CodeFox Review:\n${feedback}`;
+  // pr url - https://api.github.com/repos/AnshulKahar2729/ai-pull-request/pulls/9
+  // const url = `https://api.github.com/repos/${owner}/${repo}/issues/${prNumber}/comments`;
+
+  const user = prUrl.split("/")[4];
+  const repo = prUrl.split("/")[5];
+  const prNumber = prUrl.split("/")[7];
+
+  const commentUrl = `https://api.github.com/repos/${user}/${repo}/issues/${prNumber}/comments`;
   try {
     const { data } = await axios.post(
       commentUrl,
       { body },
-      { headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` } }
+      { headers: { Authorization: `${process.env.GITHUB_TOKEN}` } }
     );
 
     console.log("Feedback posted:", data);
     return data;
   } catch (error) {
-    console.error("Failed to post feedback:",error);
+    console.error("Failed to post feedback:", error);
   }
 }
